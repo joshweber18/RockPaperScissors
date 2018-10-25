1f10eda6ddc1a5c137a91fa11efc32c57ae8d0c8
joshweber18 <josh18weber@gmail.com>
2018-10-25 15:19:43 -0500

    Added winning comparison method.

--- ComputerPlayer.cs
+++ ComputerPlayer.cs
@@ -12,7 +12,34 @@ namespace RPSLS
         {
             Random randomNumberGenerator = new Random();
             int randomNumber = randomNumberGenerator.Next(gestureOptions.Count);
-            
+
+            switch (randomNumber)
+            {
+                case 1:
+                    gesture = gestureOptions[0];
+                    Console.WriteLine("Rock");
+                    break;
+                case 2:
+                    gesture = gestureOptions[1];
+                    Console.WriteLine("Paper");
+                    break;
+                case 3:
+                    gesture = gestureOptions[2];
+                    Console.WriteLine("Scissors");
+                    break;
+                case 4:
+                    gesture = gestureOptions[3];
+                    Console.WriteLine("Lizard");
+                    break;
+                case 5:
+                    gesture = gestureOptions[4];
+                    Console.WriteLine("Spock");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number.");
+                    break;
+            }
+
         }
     }
 }
--- Game.cs
+++ Game.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Rock crushes Scissors
+// Scissors cuts Paper
+// Paper covers Rock
+// Rock crushes Lizard
+// Lizard poisons Spock
+// Spock smashes Scissors
+// Scissors decapitates Lizard
+// Lizard eats Paper
+// Paper disproves Spock
+// Spock vaporizes Rock
+ 
+
 namespace RPSLS
 {
     class Game
@@ -11,8 +24,11 @@ namespace RPSLS
         // member variables
         public int roundCount;
         public string takeTurns;
-        public string Player1;
-        public string Player2;
+        Player Player1;
+        Player Player2;
+        public int amountOfPlayers;
+        public int ScorePlayer1;
+        public int ScorePlayer2;
 
 
         // constructor
@@ -23,20 +39,129 @@ namespace RPSLS
         {
 
         }
-        public void RoundCounter()
+
+        public void Rules() // What gesture beats what.
+        {
+            if (Player1.gesture == Player1.gestureOptions[0] && Player2.gesture == Player2.gestureOptions[2])
+            {
+                Console.WriteLine("Rock crushes Scissors");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[2] && Player2.gesture == Player2.gestureOptions[1]) 
+            {
+                Console.WriteLine("Scissors cuts Paper");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[1] && Player2.gesture == Player2.gestureOptions[0]) 
+            {
+                Console.WriteLine("Paper covers Rock");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[0] && Player2.gesture == Player2.gestureOptions[3])
+            {
+                Console.WriteLine("Rock crushes Lizard");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[3] && Player2.gesture == Player2.gestureOptions[4]) 
+            {
+                Console.WriteLine("Lizard poisons Spock");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[4] && Player2.gesture == Player2.gestureOptions[2]) 
+            {
+                Console.WriteLine("Spock smashes Scissors");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[2] && Player2.gesture == Player2.gestureOptions[3]) 
+            {
+                Console.WriteLine("Scissors decapitates Lizard");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[3] && Player2.gesture == Player2.gestureOptions[1]) 
+            {
+                Console.WriteLine("Lizard eats Paper");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[1] && Player2.gesture == Player2.gestureOptions[4]) 
+            {
+                Console.WriteLine("Paper disproves Spock");
+            }
+
+            if (Player1.gesture == Player1.gestureOptions[4] && Player2.gesture == Player2.gestureOptions[0]) 
+            {
+                Console.WriteLine("Spock vaporizes Rock");
+            }
+
+            // start of other players if statements
+
+            if (Player2.gesture == Player2.gestureOptions[0] && Player1.gesture == Player1.gestureOptions[2]) 
+            {
+                Console.WriteLine("Rock crushes Scissors");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[2] && Player1.gesture == Player1.gestureOptions[1]) 
+            {
+                Console.WriteLine("Scissors cuts Paper");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[1] && Player1.gesture == Player1.gestureOptions[0])
+            {
+                Console.WriteLine("Paper covers Rock");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[0] && Player1.gesture == Player1.gestureOptions[3]) 
+            {
+                Console.WriteLine("Rock crushes Lizard");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[3] && Player1.gesture == Player1.gestureOptions[4]) 
+            {
+                Console.WriteLine("Lizard poisons Spock");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[4] && Player1.gesture == Player1.gestureOptions[2]) 
+            {
+                Console.WriteLine("Spock smashes Scissors");
+            }
+
+            if (Player2.gesture == Player2.gestureOptions[2] && Player1.gesture == Player1.gestureOptions[3]) 
+            {
+                Console.WriteLine("Scissors decapitates Lizard");
+            }
+            if (Player2.gesture == Player2.gestureOptions[3] && Player1.gesture == Player1.gestureOptions[1]) 
+            {
+                Console.WriteLine("Lizard eats Paper");
+            }
+            if (Player2.gesture == Player2.gestureOptions[1] && Player1.gesture == Player1.gestureOptions[4]) 
+            {
+                Console.WriteLine("Paper disproves Spock");
+            }
+            if (Player2.gesture == Player2.gestureOptions[4] && Player1.gesture == Player1.gestureOptions[0]) 
+            { 
+                Console.WriteLine("Spock vaporizes Rock");
+            }
+
+        }
+
+        public void Scoreboard() // Tracks score for each player.
+        {
+
+        }
+
+        public void RoundCounter() // Changes round after each turn.
         {
 
         }
 
         public void GetPlayers()
         {
-
+            Console.WriteLine("Please enter the amount of players.");
+            amountOfPlayers = int.Parse(Console.ReadLine());
+            DeterminePlayers(amountOfPlayers);
         }
 
-        public void DeterminePlayers(int numberOfPlayers)
+        public void DeterminePlayers(int numberOfPlayers) // Human vs. Human || Human vs. Computer
         {
-            Player Player1;
-            Player Player2;
 
             if (numberOfPlayers == 1)
             {
@@ -48,11 +173,18 @@ namespace RPSLS
                 Player1 = new HumanPlayer();
                 Player2 = new HumanPlayer();
             }
-        }
-        public void GetGestures()
+            else if (numberOfPlayers == 0)
             {
-            Player1.ChooseGesture;
+                Player1 = new ComputerPlayer();
+                Player2 = new ComputerPlayer();
             }
         }
+
+        public void GetGestures()
+            {
+                Player1.ChooseGesture();
+                Player2.ChooseGesture();
+            }
+        
     }
 }
